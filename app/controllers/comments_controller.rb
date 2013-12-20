cbf2e2f8e5a6b01c0c2770f4e4aa0af4548c222c
App Academy Student <student@appacademy.io>
2013-12-20 12:21:10 -0500

    ajax for likes on posts and adding comments to posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,12 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
     @comment.post_id = params[:post_id]
     @comment.save
-    redirect_to :back
+
+    if request.xhr?
+      render partial: "layouts/comment_view", locals: { comment: @comment }
+    else
+      redirect_to :back
+    end
   end
 
   def destroy
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,23 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(params[:like])
     @like.save
-    redirect_to :back
+
+    if request.xhr?
+      render partial: "layouts/like_button_post", locals: { post: @like.likeable }
+    else
+      redirect_to :back
+    end
   end
 
   def destroy
     @like = Like.find(params[:id])
+    likeable_obj = @like.likeable
     @like.destroy
-    redirect_to :back
+
+    if request.xhr?
+      render partial: "layouts/like_button_post", locals: { post: likeable_obj }
+    else
+      redirect_to :back
+    end
   end
 end
