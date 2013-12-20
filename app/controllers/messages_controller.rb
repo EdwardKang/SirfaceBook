acbeb651d91187f735cbba3afa2553e84635128d
App Academy Student <student@appacademy.io>
2013-12-20 13:12:44 -0500

    added ajax for message children

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,27 @@
 class MessagesController < ApplicationController
   include MessagesHelper
-  
+
   before_filter :in_conversation, only: [:show]
-  
+
   def index
     @messages = Message.where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id).where(parent_id: nil)
   end
-  
+
   def show
     @message = Message.find(params[:id])
   end
-  
+
   def create
     @message = Message.new(params[:message])
     @message.save
     if @message.parent_id == nil
       redirect_to @message
     else
-      redirect_to message_url(@message.parent_id)
+      if request.xhr?
+        render partial: "messages/message", locals: { message: @message }
+      else
+        redirect_to message_url(@message.parent_id)
+      end
     end
   end
 end
