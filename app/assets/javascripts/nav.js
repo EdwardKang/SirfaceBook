6cbfb57bf076840a153ece96990856c7c14ca18e
Edward Kang <eyk23@cornell.edu>
2014-01-30 01:11:58 -0500

    finished dropdowns

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,5 +1,23 @@
 $(document).ready(function(event){
-	$(".has-dropdown").on("click", function(){
+	$(".toggle-dropdown").on("click", function(){
+		var $a = $(this);
+		var $li = $a.closest(".has-dropdown");
+		var $ul = $a.closest(".header-list");
 		
+		$ul.find(".show").toggleClass("show");
+		
+		$dropdown = $li.find(".dropdown");
+		$dropdown.toggleClass("show");
+	});
+	
+	$("body").on("click", function(){
+		var $body = $(this);
+		
+		$dropdown = $body.find(".dropdown");
+		$dropdown.removeClass("show");
+	});
+	
+	$("body").on("click", '.has-dropdown', function(e){
+		e.stopPropagation();
 	});
 });
